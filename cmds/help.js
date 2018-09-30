43017648a0e5552f9e5b5ad32ebedf7ad7a31764
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2018-09-30 18:58:37 -0500

    Updated the help command so it now works. It displays a RichEmbed of all the commands.

--- cmds/help.js
+++ cmds/help.js
@@ -1,20 +1,35 @@
 const fs = require("fs");
+const Discord = require("discord.js");
 
 /*
  This command returns a list of all commands currently available to use by the bot.
+
  Usage is defined as ~help
 */
 module.exports.run = async (bot, message, args) => {
-	message.channel.send("Here are the following commands: ");
-	message.channel.send("avatar: Get the avatar of a user with ~avatar @user");
-	message.channel.send("bestgirl: Randomly selects the best girl of the server using ~bestgirl");
-	message.channel.send("happyday: Randomly generates a happy day using ~happyday");
-	message.channel.send("moron: Tells the owner of the bot he is a moron using ~moron");
-	message.channel.send("mute: Mutes a user using ~mute @user if you have the correct permissions");
-	message.channel.send("unmute: Unmutes a user using ~unmute @user if you have the correct permissions");
-	message.channel.send("userinfo: Gives information of a user using ~userinfo @user");
+
+	//Find all of the .js files in the cmds folder.
+	fs.readdir("./cmds/", (err, files) => {
+	if (err) console.error(err);
+
+	let jsfiles = files.filter(f => f.split(".").pop() === "js");
+
+	jsfiles.forEach((f, i) => {
+		//Get rid of the .js at the end of each filename
+		jsfiles[i] = jsfiles[i].substring(0,jsfiles[i].length-3);
+	});
+
+	//Create a new RichEmbed that contains all of the commands.
+	let embed = new Discord.RichEmbed()
+					.setTitle("Commands")
+					.setDescription(jsfiles);
+
+	//Send the RichEmbed that contains all of the commands.
+	message.channel.send(embed);
+	});
+	
 }
 
 module.exports.help = {
-	name: "commands"
+	name: "help"
 }
