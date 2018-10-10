77085dc0d9caf5a58a1c97db85249d141b122036
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2018-10-10 15:09:32 -0500

    Add support for getting help with specific commands.

--- cmds/help.js
+++ cmds/help.js
@@ -7,6 +7,9 @@ const Discord = require("discord.js");
  Usage is defined as ~help
 */
 module.exports.run = async (bot, message, args) => {
+	var name = "";
+	var desc = "";
+	var usage = "";
 
 	//Find all of the .js files in the cmds folder.
 	fs.readdir("./cmds/", (err, files) => {
@@ -14,6 +17,27 @@ module.exports.run = async (bot, message, args) => {
 
 	let jsfiles = files.filter(f => f.split(".").pop() === "js");
 
+	//If the user is asking for help with a specific command.
+	if (args[0] != null) {
+		if (!jsfiles.includes(args[0] + ".js")) return message.channel.send("Please specify a valid command. Try ~help without parameters.");
+		//Grab all of the information listed for each command.
+		let props = require(`./${args[0]}`);
+		name = props.help.name;
+		desc = props.help.description;
+		usage = props.help.usage;
+
+		//Create a new RichEmbed that gives information on the specific command.
+		let embed = new Discord.RichEmbed()
+					.setTitle(`${name}`)
+					.setDescription(`${desc}`)
+					.addField("Usage", `${usage}`)
+					.setColor("f21d4b");
+
+		message.channel.send(embed);
+	}
+	else {
+
+	//Iterate through every file and make the name just the filename
 	jsfiles.forEach((f, i) => {
 		//Get rid of the .js at the end of each filename
 		jsfiles[i] = jsfiles[i].substring(0,jsfiles[i].length-3);
@@ -22,15 +46,16 @@ module.exports.run = async (bot, message, args) => {
 	//Create a new RichEmbed that contains all of the commands.
 	let embed = new Discord.RichEmbed()
 					.setTitle("Commands")
-					.setColor("FFFFFF")
+					.setColor("f21d4b")
 					.setDescription(jsfiles);
 
-	//Send the RichEmbed that contains all of the commands.
 	message.channel.send(embed);
+	}
 	});
-	
 }
 
 module.exports.help = {
-	name: "help"
+	name: "help",
+	description: "Gives information about every command.",
+	usage: "~help or ~help command"
 }
